eb4aba34ed6ede8b6669d6ff891347dffb7a9872
Sean Brody <ifonefox@users.noreply.github.com>
2018-03-04 16:27:49 -0500

    add basic CLI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let readline = require('readline');
 let smarttv = require('./cast.js');
+let argv = require('yargs').argv;
 
 async function get_user_input(userprompt){
   return new Promise(resolve => {
@@ -11,7 +12,30 @@ async function get_user_input(userprompt){
   });
 }
 async function main(){
-  let viz = new smarttv(process.env.SMARTCAST_IP, process.env.SMARTCAST_TOKEN);
-  console.log(await viz.input_current());
+  let ip, token, viz;
+  if(argv.ip !== undefined){
+    ip = argv.ip;
+  } else if(process.env.SMARTCAST_IP !== undefined){
+    ip = process.env.SMARTCAST_IP;
+  } else {
+    ip = await get_user_input('IP: ');
+  }
+  if(argv.ip !== undefined){
+    token = argv.token;
+  } else if(process.env.SMARTCAST_IP !== undefined){
+    token = process.env.SMARTCAST_TOKEN;
+  }
+  if(token === undefined){
+    viz = new smarttv(ip);
+    viz.pair(get_user_input("PIN: "));
+  } else {
+    viz = new smarttv(ip, token);
+  }
+
+  if(argv.selectInput !== undefined){
+    viz.input_set(argv.selectInput);
+  }
+
 }
+console.log(argv);
 main();
